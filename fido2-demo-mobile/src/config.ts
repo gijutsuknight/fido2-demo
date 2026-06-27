e17c2b8169635ef044e6e776ec4acf18d14b1bfe
Yeat Nai Cheng <yeatnaicheng1@gmail.com>
2026-06-27 11:22:12 +0800

    Add API configuration and logging to API requests and responses

---
+++ fido2-demo-mobile/src/config.ts
@@ -0,0 +1,10 @@
+const API_HOST = '192.168.1.11'
+const API_PORT = 8080
+
+export const config = {
+  api: {
+    host: API_HOST,
+    port: API_PORT,
+    baseUrl: process.env.EXPO_PUBLIC_API_URL ?? `http://${API_HOST}:${API_PORT}`,
+  },
+} as const
--- fido2-demo-mobile/src/lib/api.ts
+++ fido2-demo-mobile/src/lib/api.ts
@@ -1,19 +1,48 @@
-import axios from 'axios'
+import axios, { type AxiosResponse, type InternalAxiosRequestConfig } from 'axios'
+import { config } from '@/config'
 import { useAuthStore } from './store'
 
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:8080'
+const api = axios.create({ baseURL: `${config.api.baseUrl}/api` })
 
-const api = axios.create({ baseURL: `${BASE_URL}/api` })
+function getFullUrl(config: InternalAxiosRequestConfig) {
+  return axios.getUri(config)
+}
+
+function getHeaders(
+  headers: InternalAxiosRequestConfig['headers'] | AxiosResponse['headers']
+) {
+  if (!headers) return {}
+  if (typeof headers.toJSON === 'function') return headers.toJSON()
+  return headers
+}
+
+function logRequest(config: InternalAxiosRequestConfig) {
+  console.log('[REQUEST]', getFullUrl(config), getHeaders(config.headers), config.data ?? null)
+}
+
+function logResponse(response: AxiosResponse) {
+  console.log(
+    '[RESPONSE]',
+    getFullUrl(response.config),
+    getHeaders(response.headers),
+    response.data ?? null
+  )
+}
 
 api.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token
   if (token) config.headers.Authorization = `Bearer ${token}`
+  logRequest(config)
   return config
 })
 
 api.interceptors.response.use(
-  (res) => res,
+  (res) => {
+    logResponse(res)
+    return res
+  },
   (err) => {
+    if (err.response) logResponse(err.response)
     if (err.response?.status === 401) {
       useAuthStore.getState().logout()
     }
